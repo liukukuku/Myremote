192118e88b8aa946cdef8c3d709fde86d262bc33
liukuku <1925006578@qq.com>
2020-11-26 16:32:07 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@/styles/index.less'
-
+import Login from '@/pages/Login'
+import Reg from '@/pages/Reg'
+import Home from '@/pages/Home'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 ReactDOM.render(
-  <div></div>,
+  <>
+    <BrowserRouter>
+      <Switch>
+      <Route exact path="/Login" component={Login}></Route>
+      <Route exact path="/Reg" component={Reg}></Route>
+      <Route path="*" render={()=>{
+        if (sessionStorage.getItem("auth")) {
+          return (
+            <Switch>
+              <Route exact path="/home" component={Home}></Route>
+            </Switch>  
+          )
+        } else {
+          return (
+            <Redirect to="/Login"></Redirect>  
+          )
+        }
+      }}></Route>
+      </Switch>
+    </BrowserRouter>
+  </>,
   document.getElementById('root')
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div>
+            home
+        </div>
+    )
+}
+
+export default index

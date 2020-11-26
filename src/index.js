e58f91b5e9c0390d5061a38cca271da570d5d60b
likukuku <1691629316@qq.com>
2020-11-26 18:57:52 +0800

    1

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import '@/styles/index.less'
 import Login from '@/pages/Login'
 import Reg from '@/pages/Reg'
 import Home from '@/pages/Home'
-import Index from '@/pages/Index'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 ReactDOM.render(
   <>
     <BrowserRouter>
-      {/* <Switch> */}
-      {/* <Route exact path="/Login" component={Login}></Route>
+      {/* <Switch>
+      <Route exact path="/Login" component={Login}></Route>
       <Route exact path="/Reg" component={Reg}></Route>
       <Route path="*" render={()=>{
         if (sessionStorage.getItem("auth")) {
           return ( */}
             <Switch>
-              <Route exact path="/Index" component={Index}></Route>
-              <Redirect to="/Index"></Redirect> 
+              <Route exact path="/home" component={Home}></Route>
+              <Redirect to="/home"></Redirect>
             </Switch>  
           {/* )
         } else {

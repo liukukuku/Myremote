205b2533567f9b9ff2cc533fcde826c7294c63c6
liukuku <1925006578@qq.com>
2020-11-26 17:19:00 +0800

    登陆更改

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,20 +1,5 @@
 import React from 'react'
 import { Form, Input, Button, Checkbox } from 'antd';
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 5,
-  },
-};
-const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 5,
-  },
-};
-
 const Index = () => {
   const onFinish = (values) => {
     console.log('Success:', values);
@@ -25,51 +10,59 @@ const Index = () => {
   };
 
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <div className="Login">
+      <div className="MyAccountLogin">
+        <h1>My  Account  Login</h1>
+        <p>have a nice day</p>
+      </div>
+      <div className="FormLoginbox">
+        <Form
+          name="basic"
+          className="FormLogin"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password/>
+          </Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
+          <Form.Item >
+            <Button type="primary" htmlType="submit">
+              Submit
         </Button>
-      </Form.Item>
-    </Form>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 }
 export default Index

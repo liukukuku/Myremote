7100f7f8125e6143c30dbc7c7f05c146fcbc9ef2
likukuku <1691629316@qq.com>
2020-11-26 18:54:21 +0800

    1

--- src/index.js
+++ src/index.js
@@ -4,27 +4,29 @@ import '@/styles/index.less'
 import Login from '@/pages/Login'
 import Reg from '@/pages/Reg'
 import Home from '@/pages/Home'
+import Index from '@/pages/Index'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 ReactDOM.render(
   <>
     <BrowserRouter>
-      <Switch>
-      <Route exact path="/Login" component={Login}></Route>
+      {/* <Switch> */}
+      {/* <Route exact path="/Login" component={Login}></Route>
       <Route exact path="/Reg" component={Reg}></Route>
       <Route path="*" render={()=>{
         if (sessionStorage.getItem("auth")) {
-          return (
+          return ( */}
             <Switch>
-              <Route exact path="/home" component={Home}></Route>
+              <Route exact path="/Index" component={Index}></Route>
+              <Redirect to="/Index"></Redirect> 
             </Switch>  
-          )
+          {/* )
         } else {
           return (
             <Redirect to="/Login"></Redirect>  
           )
         }
       }}></Route>
-      </Switch>
+      </Switch> */}
     </BrowserRouter>
   </>,
   document.getElementById('root')
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function index() {
+function Index() {
     return (
         <div>
             home1231231
@@ -8,4 +8,4 @@ function index() {
     )
 }
 
-export default index
+export default Index
